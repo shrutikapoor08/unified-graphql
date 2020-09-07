88dd13347cb8ca9d563c822489fd6ede8e00d5b7
shrutikapoor08 <shruti.kapoor08@gmail.com>
2020-09-06 17:54:52 -0700

    adding articles api

--- functions/articles.js
+++ functions/articles.js
@@ -1,7 +1,7 @@
 
 const fetch = require("node-fetch");
 
-exports.handler = async function(event, context, callback) {
+exports.handler = async function(event, context) {
     const response = await fetch('https://dev.to/api/articles?username=shrutikapoor08');
     const dataJson = await response.json();
      console.log(dataJson[0]);
@@ -16,9 +16,9 @@ exports.handler = async function(event, context, callback) {
      */
    
      // success
-     return {
+     return JSON.stringify({
        title: dataJson[0].title,
        description: dataJson[0].description,
        id: dataJson[0].id
-     };
+     });
 }
