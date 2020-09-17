8aaa8a28dc351e0843615e1e3e3f9412051f22fc
shrutikapoor08 <shruti.kapoor08@gmail.com>
2020-09-16 22:40:37 -0700

    username error catch

--- functions/articles.js
+++ functions/articles.js
@@ -4,9 +4,8 @@ const fetch = require("node-fetch");
 exports.handler = async function(event, context, callback) {
 
     const myusername = "shrutikapoor08";
-    console.log(event);
-    const graphQLParams =  JSON.parse(event.body)
-    const username = graphQLParams.input.username || myusername;
+    const graphQLParams =  event.body ? JSON.parse(event.body) : undefined
+    const username = graphQLParams ? graphQLParams.input.username : myusername;
     const API_ENDPOINT = `https://dev.to/api/articles?username=${username}`;
     const response = await fetch(API_ENDPOINT, {
         headers: { "Accept": "application/json" }
